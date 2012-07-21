c662e37e8a75ef6a090680d48f2528c602881557
Junichi Otake <junichi.otake@gmail.com>
2012-07-21 17:16:15 +0900

    init

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 javascript-common-use
 =====================
+copyright Junichi Otake junichi.otake@gmail.com
 
 in common use. for me. メモ帳です。
+
+memomemo
+--------
+そのうち整理するかもしれません。
+使えそうな物があったらどうぞ。
+
+Image
+-----
+
+### loadImageCheckedSize
+    check image size and set image.
---
+++ javascripts/icu-image.js
@@ -0,0 +1,22 @@
+if( typeof(icu) === 'undefined' ){ icu = {}; }
+icu.image = {
+  loadImageCheckedSize: function( imgId, imageUrl ){
+    var baseSize = 100;
+    var dom = $('#'+imgId);
+    var img = new Image();
+    img.onload = function(){
+      var rate = img.height/img.width;
+      top = 0;
+      left = 0;
+      if( rate > 1 ){
+        left = parseInt((baseSize-(baseSize/rate))/2);
+      }else{
+        top = parseInt((baseSize-(baseSize*rate))/2);
+      }
+      dom.css('top', top+'px' );
+      dom.css('left', left+'px' );
+      dom.attr('src', imageUrl);
+    }
+    img.src=imageUrl;
+  }
+};
